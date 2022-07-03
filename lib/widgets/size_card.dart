b7792c80f30276918143dc2b729e96309f1e40e1
Haider <dev.haider.majid@gmail.com>
2022-07-03 22:31:21 +0300

    first commit

---
+++ lib/widgets/size_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../models/size.dart';
+import '../screens/themes.dart';
+
+class SizeCard extends StatelessWidget {
+  final Size size;
+  const SizeCard(this.size, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      child: size.isActive
+          ? Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 5,
+                horizontal: 10,
+              ),
+              decoration: BoxDecoration(
+                color: kWhiteGreyColor,
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(
+                  color: kWhiteGreyColor,
+                  width: 1.5,
+                ),
+              ),
+              child: Text(
+                size.name,
+                style: poppinsTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: kBlackColor,
+                ),
+              ),
+            )
+          : Container(
+              padding: const EdgeInsets.symmetric(
+                vertical: 5,
+                horizontal: 10,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                border: Border.all(
+                  color: kWhiteGreyColor,
+                  width: 1.5,
+                ),
+              ),
+              child: Text(
+                size.name,
+                style: poppinsTextStyle.copyWith(
+                  fontSize: 14,
+                  fontWeight: FontWeight.w400,
+                  color: kBlackColor,
+                ),
+              ),
+            ),
+    );
+  }
+}
